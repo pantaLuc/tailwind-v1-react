5cc5bb806a5d35a87c430dc02927faf86023e4c7
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-04-09 05:30:53 +0200

    adding adding heroicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,22 @@ const Navbar = () => {
     return (
         <>
           <nav>
-              <Link to="/">EGG</Link>
+              <Link to="/" className='pl-8' >
+                  EGG
+              </Link>
+              <div className='px-4 cursor-pointer md:hidden'>
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                className="h-6 w-6" 
+                fill="none" viewBox="0 0 24 24" 
+                stroke="currentColor" 
+                strokeWidth={2} 
+                >
+                <path strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="M4 6h16M4 12h16M4 18h16" />
+             </svg>
+              </div>
           </nav>  
         </>
     );
