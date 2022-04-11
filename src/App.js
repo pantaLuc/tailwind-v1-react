39191e55967a3aa72529ae2ee8d2a8ad9991e873
Luc Perin Panta <lucpanta@MacBook-Pro-de-Luc.local>
2022-04-11 19:45:36 +0200

    working with apply

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Content from './components/Content';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Hero/>
+      <Content/>
     </>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image1 from '../images/christian-coquet-p1sdBSnS3Qc-unsplash.jpg';
+import Image2 from '../images/joseph-gonzalez-fdlZBWIP0aM-unsplash.jpg';
+const Content = () => {
+    return (
+        <>
+         <div className='menu-card'>
+            <img src={Image1} alt ="eggs" className='h-full rounded mb-20 shadow' /> 
+            <div className='flex flex-col justify-center items-center'>
+                <h2 className='text-2xl mb-2'>Egg Buffing </h2>
+                <p className=''> Crispy , delicious and nutritious</p>
+                <span>$23</span>
+            </div>  
+          </div>
+          <div className='menu-card'>
+            <img src={Image2} alt ="eggs" className='h-full rounded mb-20 shadow' /> 
+            <div className='flex flex-col justify-center items-center'>
+                <h2 className='text-2xl mb-2'>Egg Buffing </h2>
+                <p className=''> Crispy , delicious and nutritious</p>
+                <span>$23</span>
+            </div>  
+          </div>
+        </>
+
+    );
+};
+
+export default Content;
